3f5f412c16d15dc524dfa106d0dacd1535884def
Alexender Ovcharenko <61220723+ddkkoda@users.noreply.github.com>
2020-02-26 12:38:47 +0600

    Initial commit

---
+++ Torgi-iOS/AppDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.swift
+//  Torgi-iOS
+//
+//  Created by Alexander Ovcharenko on 25.02.2020.
+//  Copyright © 2020 Alexander Ovcharenko. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+    
+}
+
